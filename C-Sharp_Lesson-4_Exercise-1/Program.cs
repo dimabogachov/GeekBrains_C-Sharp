a93885c643abc597b0a0ea5015aede2a7a71dfda
Дмитрий Богачев <dima1983bogachov@gmail.com>
2021-09-17 14:39:58 +0300

    Урок №4- Упражнение №1

---
+++ C-Sharp_Lesson-4_Exercise-1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Lesson_4_Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
