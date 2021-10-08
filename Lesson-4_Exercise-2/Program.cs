944334220ca4e04f3b7f9cb6e2ed946ea523f04d
Дмитрий Богачев <dima1983bogachov@gmail.com>
2021-10-08 14:26:10 +0300

    На вхое строка — набор чисел, разделенных пробелом, на выходе - число = сумма всех чисел в строке

---
+++ Lesson-4_Exercise-2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4_Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вариант №1
+            Console.WriteLine("Ведите числа через пробел: ");
+            string Numbers = Console.ReadLine();
+            var number = Numbers.Split();
+            int Sum = 0;
+            for (int i = 0; i < number.Length; i++)
+            {
+                Sum = Sum + int.Parse(number[i]);
+            };
+            Console.WriteLine(Sum);
+
+
+            // Вариант №2
+            Console.WriteLine("Ведите числа через пробел: ");
+            string[] Input = Console.ReadLine().Split(' ');
+            int sum = 0;
+            for (int i = 0; i < Input.Length; sum += Convert.ToInt32(Input[i++]))
+            {
+                
+            };
+            Console.WriteLine(sum);
+
+        }
+
+    }
+}
