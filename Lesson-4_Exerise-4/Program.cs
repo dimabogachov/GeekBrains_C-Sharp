a385d62062e468cf0c270928905475ba2c1b3c06
Дмитрий Богачев <dima1983bogachov@gmail.com>
2021-10-08 18:06:12 +0300

    Вывод элемента из последовательности "Числа Фибчоначчи" по его номеру

---
+++ Lesson-4_Exerise-4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4_Exerise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер элемента из чисел Фиббоначи:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Fibonacсi(n));
+           
+        }
+        static int Fibonacсi(int Number)
+        {
+            {
+                if (Number == 0)
+                {
+                    return 0;
+                }
+                else if (Number == 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return Fibonacсi(Number - 1) + Fibonacсi(Number - 2);
+                }
+            }
+
+
+        }
+
+
+    }
+}
