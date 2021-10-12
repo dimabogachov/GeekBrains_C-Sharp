1bba68a834d50b1e9809df215bd1985da22f7ddf
Дмитрий Богачев <dima1983bogachov@gmail.com>
2021-10-12 12:49:19 +0300

    Программа, которая при старте дописывает текущее время в файл «startup.txt»

---
+++ Lesson-5_Exercise-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lesson_5_Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Time_txt = "StartUp.txt";
+
+            var Time = DateTime.Now;
+            File.WriteAllText(Time_txt, Time.ToLongTimeString());
+
+            string OutTime = File.ReadAllText(Time_txt);
+            Console.WriteLine(OutTime);
+        }
+    }
+}
