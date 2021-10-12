4d96006a42c8aa7f815ec9c980783d6424f2ffdc
Дмитрий Богачев <dima1983bogachov@gmail.com>
2021-10-12 16:31:19 +0300

    Бинарная Сериализация Десериализация не получается!!!!

--- Lesson-5_Exercise-3/Program.cs
+++ Lesson-5_Exercise-3/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Lesson_5_Exercise_3
 {
@@ -17,12 +18,32 @@ namespace Lesson_5_Exercise_3
 
             byte[] byteStrings = System.Text.Encoding.Default.GetBytes(Strings);
 
-           
+            //Сериализация бинарных данных
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(new FileStream("NumberToBytes_#2.bin", FileMode.OpenOrCreate), byteStrings);
+
+            // Десериализация
+            using (FileStream File = new FileStream("NumberToBytes_#2.bin", FileMode.Open))
+            {
+                byte[] deserilizeNumber = (byte[])formatter.Deserialize(File);
+
+                foreach (byte item in deserilizeNumber)
+                {
+                    Console.WriteLine(deserilizeNumber[item]);
+                }
+            }
+
+            
+            /*
+            // Вывод на консоль элементов массива byte
+
             for (int i = 0; i < byteStrings.Length; i++)
             {
                 Console.Write($" {byteStrings[i]} ");
             }
             Console.WriteLine();
+           
+            // Запись массива байт в бинарный файл и чтение его содержимого с выводом на консоль
 
             File.WriteAllBytes("NumberToBytes.bin", byteStrings);
             byte[] fromFile = File.ReadAllBytes("NumberToBytes.bin");
@@ -31,7 +52,7 @@ namespace Lesson_5_Exercise_3
             {
                 Console.Write($" {fromFile[i]} ");
             }
-            
+            */
         }
     }
 }
