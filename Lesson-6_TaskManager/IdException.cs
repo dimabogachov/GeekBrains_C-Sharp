7c8bd57c18b474d9e17abab4bdb483dff0e45002
Дмитрий Богачев <dima1983bogachov@gmail.com>
2021-10-15 18:04:18 +0300

    Написать консольное приложение Task Manager, которое выводит на экран запущенные процессы и позволяет завершить указанный процесс. Предусмотреть возможность завершения процессов с помощью указания его ID или имени процесса.

---
+++ Lesson-6_TaskManager/IdException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_6_TaskManager
+{
+    public enum ErrorCodes
+    {
+        Empty
+    }
+
+    public class IdException : Exception
+    {
+        public IdException(ErrorCodes code)
+        {
+            Code = code;
+        }
+
+        public ErrorCodes Code { get; }
+    }
+}
